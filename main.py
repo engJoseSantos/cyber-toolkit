9f677385ef3b64fea865ad6cf58683080ecd3f2a
engJoseSantos <eng.josesantos97@gmail.com>
2026-08-02 15:59:49 +0200

    feat: accept URL from user and refactor code into functions

--- main.py
+++ main.py
@@ -1,25 +1,28 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://nmap.org/"
+#url = "https://nmap.org/"
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36"
 }
 
-response = requests.get(url, headers=headers)
-soup = BeautifulSoup(response.text, "html.parser")
+def get_unique_links(url):
+    response = requests.get(url, headers=headers)
+    soup = BeautifulSoup(response.text, "html.parser")
 
-#print(soup)
 
-links = []
+    links = []
 
-for a in soup.find_all("a"):
-    href = a.get("href")
-    if href:
-        links.append(href)
+    for a in soup.find_all("a"):
+        href = a.get("href")
+        if href:
+            links.append(href)
 
-unique_links = set(links)
+    return set(links)
 
-for link in unique_links:
-    print(link)
+if __name__ == "__main__":
+    url = input("Type the URL to find links: ")
+    unique_links = get_unique_links(url)
+    for link in unique_links:
+        print(link)
